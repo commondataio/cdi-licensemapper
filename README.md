9293483592d5dccf6319fc2f5d5dae569d1694dd
Ivan Begtin <ibegtin@gmail.com>
2023-06-03 11:45:58 +0300

    Added simple license matching rules and script

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cdi-licensemapper
 Data licenses mapping experiments, code and data
 
+This repositry is the part of the Common Data Index project (https://github.com/commondataio)
 
 This repository includes data dumps from Common Data Index database with frequency lists of data licenses ids, urls and names.
 
@@ -8,13 +9,25 @@ Data dumped as tsv files with field names and count of occurences to 'data' dire
 
 Code as added to the 'scripts' directory. 
 
-
 ## Notes
 
 * ArcGIS Hub, DKAN and Socrata data exported as DCAT 1.1 (dcatus11) with 'license' field
 * uData provides 'license' field as abbreviation, more research needed, maybe it could be extracted from source
 * from CKAN datasets data fields extracted, not from CKAN license API endpoint. 
 * Geonetwork schema is unstable and 'legalContraint' field could be string, dict, or string with "|" delimiter. It's parsed, script added
-* InvenioRDM include multiple rights records, all extracted
+* InvenioRDM includes multiple rights records, all extracted
 
+
+## General approach
+
+1. Select most common licenses and create list of licenses with id, name and url for each license
+2. Add mapper from license names, urls and id's to most common licenses
+3. Create simple Python function to identify license from text provided. Use simple exact text matching rules
+
+
+## Possible future development
+
+1. Create registry of common licenses in the wild and add idenitifiers linked to OpenDefinition, SPDX, DCAT-AP.de, INSPIRE registry and e.t.c, description, important features
+2. Create Python library to identify licenses types using preset of rules
+3. Link datasets and data catalogs to certain licenses and usage terms
 
---
+++ scripts/licmapper.py
@@ -0,0 +1,81 @@
+import json
+import csv
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+import typer
+
+
+app = typer.Typer()
+
+MAPPER_FILE = '../data/mapped_v1.tsv'
+LIC_FILE = '../reference/commonlicenses.yaml'
+RULES_FILE = '../reference/rules.yaml'
+
+@app.command()
+def find_missing():
+    """Identifiers which licenses in mapped file missing in the reference registry"""
+    lic_ids = []
+
+    f = open(LIC_FILE, 'r', encoding='utf8')
+    licenses = yaml.load(f, Loader=Loader)
+    f.close()
+    for lic in licenses:
+        lic_ids.append(lic['id'])
+
+    unmapped = []
+    f = open(MAPPER_FILE, 'r', encoding='utf8')
+    reader = csv.DictReader(f, delimiter='\t') 
+    for row in reader:
+        if row['license_id'] not in lic_ids:
+            if row['license_id'] not in unmapped:
+                unmapped.append(row['license_id'])
+    f.close()
+
+    print(unmapped)
+
+@app.command()
+def convert():
+    """Convert mapped data to yaml rules"""
+    data = []
+    texts = []
+    f = open(MAPPER_FILE, 'r', encoding='utf8')
+    reader = csv.DictReader(f, delimiter='\t') 
+    for row in reader:
+        if row['text'] in texts: continue
+        texts.append(row['text'])
+        data.append({'text': row['text'], 'id' : row['id']})
+    f.close()
+    f = open(RULES_FILE, 'w', encoding='utf8')
+    f.write(yaml.dump(data, Dumper=Dumper))
+    f.close()
+
+@app.command()
+def identify(text):
+    """Identifies license by text provided. Very slow and ineffective. Temporary solution test license mapping for Common Data Index"""
+    licenses = {}
+    f = open(LIC_FILE, 'r', encoding='utf8')
+    licenses_data = yaml.load(f, Loader=Loader)
+    f.close()
+    for row in licenses_data:
+        licenses[row['id']] = row
+    
+    rules  = {}
+    f = open(RULES_FILE, 'r', encoding='utf8')
+    rules_data = yaml.load(f, Loader=Loader)
+    f.close()
+    for row in rules_data:
+        rules[row['text']] = row
+
+    if text in rules.keys():
+       license = licenses[rules[text]['id']]
+       print('License id: %s, name: %s for text - %s' % (license['id'], license['name'], text))
+    else:
+       print('License not found')
+
+if __name__ == "__main__":
+    app()
